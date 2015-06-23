e87e28a2a862e18aa248125d5bf5cc333f4b94bf
John McGillion <john.r.mcgillion@gmail.com>
2015-06-23 15:01:32 -0400

    Saving the calculator ...

--- Week1_Tealeaf/calculator.rb
+++ Week1_Tealeaf/calculator.rb
@@ -1,24 +1,44 @@
-puts "Please enter Numero Uno?"
+def say(message)
+	puts "#{message}"
+end
+
+say "Please enter Numero Uno?"
 
 num1 = gets.chomp
 
-puts "Please enter #2?"
+say "The first number that has been chosen is #{num1}"
+
+say "Please enter #2?"
 
 num2 = gets.chomp
 
-puts "Please enter: (1) to add, (2) to subtract, (3) to multiply and (4) to divide."
+say "The second # that has been chosen is #{num2}."
+
+say "Please enter: (1) to add, (2) to subtract, (3) to multiply and (4) to divide."
 
 method = gets.chomp.to_i
 
 if method == 1
-  puts num1.to_i + num2.to_i
+	say "OK.  So, we will be adding ... "
+	result = num1.to_i + num2.to_i
 elsif method == 2
-	puts num1.to_i - num2.to_i
+	say "OK.  So, we will be subtracting ... "
+	result = num1.to_i - num2.to_i
 elsif method == 3
-	puts num1.to_i * num2.to_i
+	say "OK.  So, we will be multiplying ... "
+	result = num1.to_i * num2.to_i
 elsif method == 4
-	puts num1.to_f / num2.to_i
+	say "OK.  So, we will be dividing ... "
+	result = num1.to_f / num2.to_f
 end
+
+say "The result is #{result}"
+
+
+
+
+
+
 
 
 
